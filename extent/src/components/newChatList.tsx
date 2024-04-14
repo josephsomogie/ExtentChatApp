03cf7805e04d855bd9f1e66309ed67a166b27398
josephsomogie <128629560+josephsomogie@users.noreply.github.com>
2024-04-14 11:58:11 -0400

    UI new chat update

--- extent/src/components/newChatList.tsx
+++ extent/src/components/newChatList.tsx
@@ -3,6 +3,7 @@ import pushConversations from "~/FrontendApiCalls/pushConversation";
 import { User } from "~/ClientFunctions/interfaces";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
+import { Check, Search, X } from "lucide-react";
 export default function NewList(){
   const [users, setUsers] = useState<User[] |  null>(null);
   const [query, setQuery] = useState("");
@@ -17,6 +18,14 @@ export default function NewList(){
     console.log('my Name:'+myName)
     console.log("my ID: "+myId)
   }
+  const onEnter = (e: any) => {
+    
+    if(e.key === 'Enter'){
+      e.preventDefault();
+      loadQuery();
+    }
+  }
+
   const  {data : session} = useSession();
   const [myName, setMyName] = useState <String | null>(session?.user.name || null )
   const [myId, setMyId] = useState <String | null>(session?.user.id || null )
@@ -34,26 +43,33 @@ export default function NewList(){
           <form
           onSubmit={loadQuery}
           >
+           <div className='flex flex-row border-b'>
             <textarea 
-            className='rounded-sm resize-none'
+            className='rounded-sm resize-none w-full px-2 dark:placeholder-white placeholder-black dark:bg-transparent place-content-center h-1/3 dark:text-white outline-none  '
             placeholder="Search by username"
             value={query}
             onChange={(e)=>{setQuery(e.target.value)}}
-            />
+            onKeyDown={(e)=>onEnter(e)}
+            >
+              
+            </textarea>
+            <Search 
+              style={{color:'white', marginTop:12, marginLeft:6, backgroundColor:'transparent ', }}
+              onClick={loadQuery}
+              
+              />
+            </div>
             
           </form>
-          <button
-        onClick={loadQuery}
-        className=' m-2 p-1 rounded-md text-white bg-blue-500'
-        >search</button>
+         
         </div>
-        <div className=''>
+        <div className='py-2'>
           {users ? users.map((user:User) =>(
             <div>
             <div
               key={user.id}
-              onClick={toggleModal/*() => pushConversations(user.name, [user.id, myId], myId)*/}
-              className=" mb-2 cursor-pointer rounded-full bg-blue-500 px-4 py-2"
+              onClick={toggleModal}
+              className=" mx-2 mb-2 cursor-pointer rounded-sm bg-gray-600 hover:bg-gray-400 px-4 py-2 text-white"
             >
               {user.name}
             </div>
@@ -62,7 +78,7 @@ export default function NewList(){
             <Modal isOpen={isModalOpen} toggle={toggleModal} user={user} id={myId}/>
           </div>
           
-          )): <p>Make a search...</p>}
+          )): <p className='dark:text-white pt-4'>0 Results...</p>}
         </div>
         
       </center>
@@ -97,16 +113,16 @@ const [convoName, setConvoName] = useState<string | null>(null)
       
       {isOpen && (
         <dialog onClick={(e)=>{handleBackgroundClick(e)}}className="fixed left-0 top-0 w-full h-full rounded-md bg-black bg-opacity-50 z-50 overflow-auto  flex justify-center items-center">
-          <div className="bg-gray-200 m-auto p-8">
+          <div className="bg-slate-500 m-auto p-8">
             <div className="flex flex-col items-center">
               <h3>New Conversation with  {user.name}</h3>
               <br/>
               <input placeholder="conversation name"
               onChange={(e)=>{setConvoName(e.target.value)}}
               ></input>
-              <div>
-              <button onClick={handleSubmit} className=" bg-blue-500 m-2 rounded-md text-white p-2">create</button>
-              <button onClick={toggle} className=" bg-red-700 m-2 rounded-md text-white p-2">Cancel</button>
+              <div className='flex flex-row py-2'>
+              <Check onClick= {handleSubmit} style={{ color:'green', marginRight:25}}/>
+              <X onClick={toggle} style={{color:'red'}}/>
               </div>
             </div>
           </div>
